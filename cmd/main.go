5a23e80f9e254eb2798201a56d4d385a7a4fbb58
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-19 13:19:41 +0300

    use log module to exit with code 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"github.com/daronenko/calc/internal/calculator"
 )
 
 func main() {
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "You need to pass mathematical expression to calculate.\n")
-		os.Exit(1)
+		log.Fatalf("You need to pass mathematical expression to calculate.\n")
 	}
 	expression := os.Args[1]
 
 	result, err := calculator.Eval(expression)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("error: %v\n", err)
 	}
 
-	fmt.Println(result)
+	log.Println(result)
 }
